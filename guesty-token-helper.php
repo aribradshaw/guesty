e1e3a32cd99c7216deadb8aba8500c7d61c47832
Ari Bradshaw <ari.bradshaw@vanderbilt.edu>
2025-06-26 20:26:38 -0700

    2.12 flatpickr 3 Month Calendar Integration

--- guesty-token-helper.php
+++ guesty-token-helper.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: MannaGuesty by The Manna Agency and Flygon LC
  * Description: Securely handles Guesty API bearer token and exposes it to front-end JavaScript. Allows various Guesty API shortcode calls.
- * Version: 2.11 - URL Parameters for Checkin and Checkout
+ * Version: 2.12 - flatpickr 3 Month Calendar Integration
  * Author: Ari Daniel Bradshaw - Flygon LC & Dan Park - The Manna Agency
  */
 
